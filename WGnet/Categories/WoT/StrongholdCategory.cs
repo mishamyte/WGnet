d7fccea36296ebcec22c31b2660fe07887089111
mishamyte <myte@ukr.net>
2015-06-13 20:27:19 +0300

    Реализован метод "stronghold/accountstats"

--- WGnet/Categories/WoT/StrongholdCategory.cs
+++ WGnet/Categories/WoT/StrongholdCategory.cs
@@ -72,5 +72,63 @@ namespace WGnet.Categories.WoT
 
             return new ReadOnlyDictionary<int, StrongholdBuildings>(obj.Data);
         }
+
+        /// <summary>
+        /// Метод возвращает статистику игрока по Укрепрайону текущего клана
+        /// </summary>
+        /// <param name="accountId">Идентификатор аккаунта игрока</param>
+        /// <param name="accessToken">Ключ доступа к персональным данным пользователя. Имеет срок действия. Для получения ключа доступа необходимо запросить аутентификацию.</param>
+        /// <param name="language">Язык локализации</param>
+        /// <param name="fields">Поля ответа. Поля разделяются запятыми. Вложенные поля разделяются точками. Для исключения поля используется знак «-» перед названием поля. Если параметр не указан, возвращаются все поля</param>
+        /// <returns>Словарь, ключ - id клана, значение - статистика игрока по Укрепрайону текущего клана</returns>
+        public ReadOnlyDictionary<long, StrongholdAccountStats> AccountStats(long accountId, string accessToken = null,
+            LanguageField? language = null, string fields = null)
+        {
+            var parameters = new WgParameters
+                             {
+                                 {"account_id", accountId},
+                                 {"access_token", accessToken},
+                                 {"fields", fields},
+                             };
+
+            if (language != null)
+                parameters.Add("language", Enum.GetName(typeof(LanguageField), language).ToLower());
+
+
+            var response = _wg.Call("stronghold/accountstats/", WgSection.WoT, parameters);
+
+            var obj = JsonConvert.DeserializeObject<WgResponse<Dictionary<long, StrongholdAccountStats>>>(response);
+
+            return new ReadOnlyDictionary<long, StrongholdAccountStats>(obj.Data);
+        }
+
+        /// <summary>
+        /// Метод возвращает статистику игрока по Укрепрайону текущего клана
+        /// </summary>
+        /// <param name="accountId">Идентификатор аккаунта игрока</param>
+        /// <param name="accessToken">Ключ доступа к персональным данным пользователя. Имеет срок действия. Для получения ключа доступа необходимо запросить аутентификацию.</param>
+        /// <param name="language">Язык локализации</param>
+        /// <param name="fields">Поля ответа. Поля разделяются запятыми. Вложенные поля разделяются точками. Для исключения поля используется знак «-» перед названием поля. Если параметр не указан, возвращаются все поля</param>
+        /// <returns>Словарь, ключ - id клана, значение - статистика игрока по Укрепрайону текущего клана</returns>
+        public ReadOnlyDictionary<long, StrongholdAccountStats> AccountStats(List<long> accountId, string accessToken = null,
+            LanguageField? language = null, string fields = null)
+        {
+            var parameters = new WgParameters
+                             {
+                                 {"account_id", accountId.ToWgParameter()},
+                                 {"access_token", accessToken},
+                                 {"fields", fields},
+                             };
+
+            if (language != null)
+                parameters.Add("language", Enum.GetName(typeof(LanguageField), language).ToLower());
+
+
+            var response = _wg.Call("stronghold/accountstats/", WgSection.WoT, parameters);
+
+            var obj = JsonConvert.DeserializeObject<WgResponse<Dictionary<long, StrongholdAccountStats>>>(response);
+
+            return new ReadOnlyDictionary<long, StrongholdAccountStats>(obj.Data);
+        }
     }
 }
---
+++ WGnet/Model/WoT/Stronghold/StrongholdAccountStats.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace WGnet.Model.WoT.Stronghold
+{
+    /// <summary>
+    /// Cтатистикf игрока по Укрепрайону текущего клана
+    /// </summary>
+    public class StrongholdAccountStats
+    {
+        /// <summary>
+        /// Идентификатор аккаунта игрока
+        /// </summary>
+        [JsonProperty("account_id")]
+        public long AccountId { get; set; }
+
+        /// <summary>
+        /// Идентификатор клана игрока
+        /// </summary>
+        [JsonProperty("clan_id")]
+        public long ClanId { get; set; }
+
+        /// <summary>
+        /// Статистика боёв игрока в режиме обороны в текущем клане. Если у клана игрока нет Укрепрайона, полю присваивается значение null.
+        /// См. <see cref="StrongholdAccountStatsDefense"/>
+        /// </summary>
+        [JsonProperty("stronghold_defense")]
+        public StrongholdAccountStatsDefense StrongholdDefense { get; set; }
+
+        /// <summary>
+        /// Статистика боёв игрока в режиме вылазок в текущем клане. Если у клана игрока нет Укрепрайона, полю присваивается значение null.
+        /// См. <see cref="StrongholdAccountStatsSkirmish"/>
+        /// </summary>
+        [JsonProperty("stronghold_skirmish")]
+        public StrongholdAccountStatsSkirmish StrongholdSkirmish { get; set; }
+    }
+}
